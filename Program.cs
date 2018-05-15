6bb0dff280f5475af5028749a3e684aea508e8b8
Garrett <garrettgucci@gmail.com>
2018-05-15 16:40:23 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           List<object> boxList = new List<object>();
+           boxList.Add(7);
+            boxList.Add(28);
+            boxList.Add(-1);
+            boxList.Add(true);
+            boxList.Add("chair");
+
+            foreach (var obj in boxList){
+                Console.WriteLine(obj);
+            }
+            int sum = 0;
+
+            foreach (var obj in boxList){
+                if (obj is int){
+                    int castInt = (int)obj;
+                    sum += castInt;
+                }
+            }
+            Console.WriteLine("Sum of Integers: {0}", sum);
+        }
+    }
+}
